05676bc3740d4c391dc6d3d61be9301254cdade5
BackSage <rafael.ferafa@gmail.com>
2026-01-08 20:50:50 -0300

    aprendendo a passar valores de duas activities diferentes

---
+++ kotlin-course-activities/Bundlecomextras/app/src/main/java/com/example/bundlecomextras/MainActivity2.kt
@@ -0,0 +1,35 @@
+package com.example.bundlecomextras
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bundlecomextras.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val i = intent
+        val bundle = i.extras
+        binding.buttonOk.setOnClickListener {
+            val numero = binding.editNumero.text.toString().trim().toInt()
+            val i = Intent(this, MainActivity3::class.java)
+            i.putExtra("numero2", numero)
+            if (bundle != null){
+            i.putExtras(bundle)
+            }
+            startActivity(i)
+        }
+    }
+}
